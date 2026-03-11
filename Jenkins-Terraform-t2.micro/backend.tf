64b785c97e406f9153ec1bade37dab462b240edf
Siddarth <siddarthrsurya@gmail.com>
2026-03-11 10:29:40 +0530

    Delete Jenkins-Terraform-t2.micro directory

--- Jenkins-Terraform-t2.micro/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-siddu-1"
-    key            = "Jenkins-Terraform-t2.micro/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
