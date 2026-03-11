e7bfb437d127d7e6306cb181df8118acb132d6f8
Siddu <siddarthrsurya@gmail.com>
2026-03-11 11:29:14 +0530

    added backend.tf

--- Jenkins-Terraform/backend.tf
+++ Jenkins-Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-siddu-1"
-    key            = "Jenkins-Terraform-t2.micro/terraform.tfstate"
+    key            = "Jenkins-Terraform/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
   }
