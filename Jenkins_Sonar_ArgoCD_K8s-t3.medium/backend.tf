246b1366d8ed08a356ba5a86062d7bdf1eb1aa44
Siddu <siddarthrsurya@gmail.com>
2026-03-12 12:10:40 +0530

    sample

--- Jenkins_Sonar_ArgoCD_K8s-t3.medium/backend.tf
+++ Jenkins_Sonar_ArgoCD_K8s-t3.medium/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "Jenkins_Sonar_ArgoCD_K8s-t3.medium/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
-    encrypt        = true
+    encrypt        = truee
   }
 }
